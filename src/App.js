c315d3d2c7264381b68a0a157fdffcf527ce4017
Sigrid Marita Kvamme <sigridkvamme@gmail.com>
2018-10-29 12:51:17 +0100

    Initialize prosject with create-react-app and clean it up

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <header>
+          <h1>Pokédex</h1>
+          <span>Add more header info here if needed</span>
+        </header>
+        <main>
+          Add pokedex functionality here.
+        </main>
+        <footer>
+          Add any footer info here if needed, like where the dataset is from and link to gitlab
+        </footer>
+      </>
+    );
+  }
+}
+
+export default App;
