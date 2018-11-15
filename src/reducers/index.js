a51b774a21eeb65c729dc010d2f5363c7b4d5966
adamgaidi <adamgaidi@outlook.com>
2018-11-15 08:56:49 +0100

    #32 removed unused files

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,14 +6,10 @@ import togglePokemonDetails from "./togglePokemonDetails";
 import offset from "./offset";
 import toggleStarPokemon from "./toggleStarPokemon";
 
-
 // All reducers need to be imported and put in here
 export default combineReducers({
   togglePokemonDetails,
   offset,
-  pokemon,
-  test,
-  test2,
   toggleStarPokemon,
   countPokemonResults,
   form: reduxFormReducer
