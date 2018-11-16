c58bcfa6e37b776ce2ae2983f5c4427b4cc98fc1
piiingz <ingviun@stud.ntnu.no>
2018-11-16 13:20:33 +0100

    #12 Added comments

--- src/components/Pill.js
+++ src/components/Pill.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./css/Pill.css";
 
+/**
+ * A component representing the type of the Pokemon
+ */
+
 const Pill = ({ color, text }) => {
   return (
     <span className="Pill" style={{ background: color }}>
